2553f4c3cfe4fe014616e0aede259d0602a6aa18
Brendan McGuire <brendan@bren.app>
2021-07-06 17:46:27 -0400

    Add form controls

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
-import('./pkg')
+import("./pkg")
+  .then((wasm) => {
+    const canvas = document.getElementById("canvas");
+    const context = canvas.getContext("2d");
+
+    const regenerate = document.getElementById("regenerate");
+    const points = document.getElementById("points");
+
+    regenerate.addEventListener("click", () => {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      wasm.generate(context, +points.value);
+    });
+
+    points.addEventListener("input", () => {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      wasm.generate(context, +points.value);
+    })
+
+    wasm.generate(context, +points.value);
+  })
   .catch(console.error);
