cd647714ae6d8e77121b66ee98e34d83b9ee54fc
Brendan McGuire <brendan@bren.app>
2021-07-18 15:24:45 -0400

    K means clustering progress

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,65 @@
 use rand::prelude::*;
+use std::convert::TryInto;
 use std::f64;
 use std::u32;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
+use cached::proc_macro::cached;
+
+#[derive(Clone, Copy)]
+pub struct Point { x: u32, y: u32 }
+
+impl Point {
+    pub fn nearest_site(&self, sites: &Vec<Point>, metric: Metric) -> Point {
+
+        let mut site = sites[0];
+        let mut min_distance = 0;
+
+        for candidate in sites {
+            let distance = metric.compute(candidate, self);
+
+            if distance < min_distance {
+                site = *candidate;
+                min_distance = distance;
+            }
+        };
+
+        site
+    }
+}
+
+
+pub enum Metric {
+    Euclidean,
+    Manhattan,
+}
+
+impl Metric {
+    pub fn compute(&self, a: &Point, b: &Point) -> u32 {
+        
+        let (a_x, a_y): (i64, i64) = (a.x.into(), a.y.into());
+        let (b_x, b_y): (i64, i64) = (b.x.into(), b.y.into());
+
+        let x = a_x - b_x;
+        let y = a_y - b_y;
+
+        let result = match self {
+            &Metric::Euclidean => x * x + y * y,
+            &Metric::Manhattan => x.abs() + y.abs(),
+        };
+
+        result.try_into().unwrap()
+    }
+}
+
+
 
 /// Generates a random point on the board
-pub fn random_point(width: u32, height: u32, rng: &mut ThreadRng) -> (u32, u32) {
+pub fn random_point(width: u32, height: u32, rng: &mut ThreadRng) -> Point {
     let x = rng.gen_range(0..width);
     let y = rng.gen_range(0..height);
 
-    (x, y)
+    Point { x, y }
 }
 
 #[wasm_bindgen]
@@ -23,7 +73,7 @@ pub fn generate(context: web_sys::CanvasRenderingContext2d, num_points: u32) {
         .unwrap();
 
     // Generate the initial points to base each region off of
-    let points: Vec<(u32, u32)> = (0..num_points)
+    let sites: Vec<Point> = (0..num_points)
         .map(|_| random_point(canvas.width(), canvas.height(), &mut rng))
         .collect();
 
@@ -33,49 +83,13 @@ pub fn generate(context: web_sys::CanvasRenderingContext2d, num_points: u32) {
     context.set_line_width(4.0);
 
     // Render each point as a little circle, and draw guidance lines to each
-    for (x, y) in &points {
-        let (x, y): (f64, f64) = ((*x).into(), (*y).into());
+    for point in &sites {
+        let (x, y): (f64, f64) = (point.x.into(), point.y.into());
 
         context.begin_path();
         context.arc(x, y, 6.0, 0.0, f64::consts::PI * 2.0).unwrap();
         context.stroke();
-
-        // Compute the perpendicular bisector for every other point
-        context.set_stroke_style(&JsValue::from_str("rgba(250, 177, 160, 0.1)"));
-        context.set_line_width(3.0);
-        for (x_1, y_1) in &points {
-            let (x_1, y_1): (f64, f64) = ((*x_1).into(), (*y_1).into());
-
-            // Compute the midpoint
-            let (x_m, y_m) = ((x + x_1) / 2.0, (y + y_1) / 2.0);
-
-            // Compute the slope and find the negative reciprocal
-            let slope = (y_1 - y) / (x_1 - x);
-            let slope_inv = -1.0 / slope;
-
-            // The function of the perpendicular bisector is now known
-            //  y - y_m = slope_inv(x - x_m)
-            //  y = slope_inv(x - x_m) + y_m
-
-            let (x_a, y_a) = (0.0, slope_inv * (0.0 - x_m) + y_m);
-            let (x_b, y_b) = (1599.0, slope_inv * (1599.0 - x_m) + y_m);
-
-            context.begin_path();
-            context.move_to(x_a, y_a);
-            context.line_to(x_b, y_b);
-            context.stroke();
-        }
-        context.set_stroke_style(&JsValue::from_str("#fab1a0"));
     }
-}
-
-#[wasm_bindgen(start)]
-pub fn start() {
-    let document = web_sys::window().unwrap().document().unwrap();
-    let canvas = document.get_element_by_id("canvas").unwrap();
-    let canvas: web_sys::HtmlCanvasElement = canvas
-        .dyn_into::<web_sys::HtmlCanvasElement>()
-        .map_err(|_| ())
-        .unwrap();
 
 }
+
