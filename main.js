ed78f25bfcfa83cb63c52f61820d7f567c7442f5
Brendan McGuire <brendan@bren.app>
2021-07-23 19:08:58 -0400

    Basic Demo: Highlist nearest site

--- main.js
+++ main.js
@@ -1,27 +1,34 @@
 import "./main.css"
 
+function demo1(wasm) {
+  const canvas = document.getElementById("demo-1");
+  const context = canvas.getContext("2d");
+
+  const regenerate = document.getElementById("demo-1-regenerate");
+  const points = document.getElementById("demo-1-points");
+
+  regenerate.addEventListener("click", () => {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    wasm.demo_basic(canvas, +points.value);
+  });
+
+  points.addEventListener("input", () => {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    wasm.demo_basic(canvas, +points.value);
+  });
+
+  wasm.demo_basic(canvas, +points.value);
+}
+
 
 import("./pkg")
   .then((wasm) => {
 
+    // Set up good error reporting
     wasm.initialize();
 
-    const canvas = document.getElementById("canvas");
-    const context = canvas.getContext("2d");
+    // Demo 1: Highlight the closest point
+    demo1(wasm);
 
-    const regenerate = document.getElementById("regenerate");
-    const points = document.getElementById("points");
-
-    regenerate.addEventListener("click", () => {
-      context.clearRect(0, 0, canvas.width, canvas.height);
-      wasm.generate(context, +points.value);
-    });
-
-    points.addEventListener("input", () => {
-      context.clearRect(0, 0, canvas.width, canvas.height);
-      wasm.generate(context, +points.value);
-    })
-
-    wasm.generate(context, +points.value);
   })
   .catch(console.error);
